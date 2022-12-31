87099089b60bb21926d41d7173b8643872d56a36
mehranShG <mehranfmpx@gmail.com>
2022-12-31 15:24:44 +0330

    feature: class validator added

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   /** Loading swagger */
   const config = new DocumentBuilder()
     .setTitle('Cerberus')
     .setDescription('User authentication api')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
-  await app.listen(3000);
-  console.log('Listening to %s', await app.getUrl());
+  /** Class validator */
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(3000)
+  console.log('Listening to %s', await app.getUrl())
 }
-bootstrap();
+bootstrap()
