b9d81eb8f2081d5e7992e7d2a169d402da9a3fb6
mehranShG <mehranfmpx@gmail.com>
2022-12-31 14:30:32 +0330

    feature: swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** Loading swagger */
+  const config = new DocumentBuilder()
+    .setTitle('Cerberus')
+    .setDescription('User authentication api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log('Listening to %s', await app.getUrl());
 }
 bootstrap();
