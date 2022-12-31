8b6f187ba6accae5a4271676fb5ea5768d1f6a9e
mehranShG <mehranfmpx@gmail.com>
2022-12-31 15:18:46 +0330

    update: auth service dependencies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { AuthEntity } from 'src/entities/auth.entity'
+import { UserEntity } from 'src/entities/user.entity'
+import { Repository } from 'typeorm'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(AuthEntity)
+    private readonly authRepository: Repository<AuthEntity>,
+  ) {}
+}
