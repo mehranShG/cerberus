991d92d26b8638208053fc58844f8afba2ca7dff
mehranShG <mehranfmpx@gmail.com>
2023-01-03 11:42:02 +0330

    update: jwt functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,18 @@
 import { AuthEntity } from 'src/entities/auth.entity'
 import { UserEntity } from 'src/entities/user.entity'
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, AuthEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, AuthEntity]),
+    JwtModule.register({ privateKey: '1234' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
