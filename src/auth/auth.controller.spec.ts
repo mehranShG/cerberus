2b154fd90a8d34d22f09f94731ff75a790c096d1
mehranShG <mehranfmpx@gmail.com>
2023-01-05 10:42:28 +0330

    update: auth controller unit testing login

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { LoginDto } from '../dtos/login.dto'
 import { RegisterDto } from '../dtos/register.dto'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 const mockAuthService = {
   register: jest.fn().mockResolvedValue({}),
+  login: jest.fn().mockResolvedValue({}),
 }
 describe('AuthController', () => {
   let controller: AuthController
@@ -29,6 +31,13 @@ describe('AuthController', () => {
     it('should register user', async () => {
       const user = new RegisterDto()
       expect(await controller.register(user)).toEqual({})
+    })
+  })
+
+  describe('login', () => {
+    it('should login', async () => {
+      const user = new LoginDto()
+      expect(await controller.login(user)).toEqual({})
     })
   })
 })
